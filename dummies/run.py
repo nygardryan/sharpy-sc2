0aea81e13f6d20e222af3b37811a03664f98e302
nygardryan <nygardryan@yahoo.com>
2020-09-13 13:33:33 -0600

    q

--- dummies/run.py
+++ dummies/run.py
@@ -1,6 +1,6 @@
-from ladder import run_ladder_game
-from dummy.dummy import LadderBot
+from dummies.protoss.macro_stalkers import LadderBot
 from sc2.player import Bot
+from ladder import run_ladder_game
 
 dummy_bot = LadderBot()
 race = dummy_bot.my_race
